117afb044a786625fccaa2c6b3e675c07f30e9a3
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2023-06-23 19:19:08 +1000

    navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import { FiInstagram } from 'react-icons/fi';
+import { BsScissors } from 'react-icons/bs'
 
 import images from '../../constants/images';
 
@@ -26,10 +27,14 @@ const Navbar = () => {
       </ul>
 
       <div className="app__navbar-login">
-        {/* Make it an instagram logo instead */}
-        <a href="#login" className='p__opensans'>Log In / Register</a>
+        {/* <a href="https://www.instagram.com/blurryvision.kk/" target="_blank" className='p__opensans'><FiInstagram/></a> */}
+        <FiInstagram color='#fff' fontSize={18} 
+          onClick={(e) => { e.preventDefault();
+          window.open('https://www.instagram.com/blurryvision.kk/','_blank').focus();
+        }} />
         <div />
-        <a href="/" className='p__opensans'>Book Table</a>
+        {/* link later to 'newletter' */}
+        <a href="/" className='p__opensans'>Bookings</a> 
       </div>
 
       <div className="app__navbar-smallscreen">
@@ -37,13 +42,13 @@ const Navbar = () => {
 
         {toggleMenu && (
         <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+          <BsScissors fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
           <ul className='app__navbar-smallscreen_links'>
             <li className='p__opensans'><a href="#home">Home</a></li>
             <li className='p__opensans'><a href="#about">About</a></li>
-            <li className='p__opensans'><a href="#menu">Menu</a></li>
-            <li className='p__opensans'><a href="#awards">Awards</a></li>
-            <li className='p__opensans'><a href="#contant">Contact</a></li>
+            <li className='p__opensans'><a href="#menu">Services</a></li>
+            <li className='p__opensans'><a href="#awards">Rules</a></li>
+            <li className='p__opensans'><a href="#contact">Contact</a></li>
           </ul>
         </div>
         )}

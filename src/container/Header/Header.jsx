c2a2c8cbba15dad2cb7ebc40c7fea4e019e8da58
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2023-06-25 20:41:27 +1000

    header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     
     <div className='app__wrapper_info'>
-      <SubHeading title="Chase the new Flavour" />
-      <h1 className='app__header-h1'>The Key to Find Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0'}} > Lorem ipsum dolor sit amet. In voluptate tempore aut eius libero eum omnis molestias in aspernatur laboriosam. In sapiente rerum quo obcaecati praesentium et recusandae blanditiis a molestiae dolorem sed cumque quasi. </p>
-      <button type='button' className='custom__button'>Explore Menu</button>
+      <SubHeading title="Blurry Vision" />
+      <h1 className='app__header-h1'>Glen Waverley's Best Barber</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}} > Come for the vibes, Come back for the fades.  </p>
+      <button type='button' className='custom__button' onClick='#services'>Explore Menu</button>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={ images.welcome }alt="" />
+      <img src={ images.trim }alt="welcome" />
     </div>
 
   </div>

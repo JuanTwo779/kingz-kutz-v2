0dbd5c87dd9057c0515c9114b2612d42d19b40c0
Juan A Mangubat <jman0023@student.monash.edu>
2023-09-01 00:41:08 +1000

    refactor: comment change

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -9,7 +9,8 @@ const FindUs = () => (
       <SubHeading title='Contact' />
       <h1 className='headtext__cormorant' style={{marginBottom: '2rem'}}>Find Us</h1>
       <div className='app__wrapper-content'>
-        <p className='p__opensans'> Our address is 7 Garwain Parade, Glen Waverley. Use the google map to help find us. </p>
+        <p className='p__opensans'> 
+Our location can be found at 7 Garwain Parade, Glen Waverley. Feel free to utilize Google Maps for assistance in locating us accurately.</p>
         <p className='p__cormorant' style={{color: 'var(--color-accent)', margin: '2rem 0 0.5rem 0'}}>Working Hours</p>
         <p className='p__opensans'>Mon - Fri: 9:00am - 5:00pm</p>
         <p className='p__opensans'>Weekends: 9:00am - 5:00pm</p>

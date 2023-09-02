7ce7c9cb424c054610e9fe6ab19a68602dec63cf
Juan A Mangubat <jman0023@student.monash.edu>
2023-09-03 00:47:58 +1000

    refactor: header refinement

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { images } from '../../constants';
+// import { SubHeading } from '../../components';
 
 import './Hero.css';
 
@@ -12,13 +13,11 @@ const Hero = () => (
                </div>
 
                {/* <SubHeading title="blurryVision.kk" /> */}
-               <h1 className='app__header-h1'>Welcome to Kingz Kutz</h1>
-               <p className="p__opensans"  style={{ margin: '1rem 0 2rem 0'}}> 
+               <h1 className='app__hero-h1'>Kingz Kutz Barber</h1>
+               <hr className='style-eight'/>
+               <p className="p__opensans app__hero-desc"> 
                     Your Kingdom for premier barber services. 
                     Serving royalty, one cut at a time.
-                    Experience the Epitome of Grooming Excellence. 
-                    Elevate Your Style with Our Regal Barber Craftsmanship. 
-                    Embrace Distinction at Every Trim.
                </p>
                {/* <a href="#services" type='button' className='custom__button'>Explore Services</a> */}
                     

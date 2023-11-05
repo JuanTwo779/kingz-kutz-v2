97053cad32fd0c16ed189c6d9e7c6ec0b676f46d
Juan A Mangubat <jman0023@student.monash.edu>
2023-11-05 23:47:33 +1100

    scissor img changes

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => (
       <div className="app__footer-links_logo">
         <img src={images.heroLogoKK} alt="footer_logo" />
         <p className="p__opensans"></p>
-        <img src={images.scissors} className="subheading__img" style={{ marginTop: 15 }} alt='scissors' />
+        <img src={images.scissors} className='footer__scissor' alt='scissors' />
         <div className="app__footer-links_icons">
           <FiFacebook 
             onClick={(e) => { e.preventDefault();

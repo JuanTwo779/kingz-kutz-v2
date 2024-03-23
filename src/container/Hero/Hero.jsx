10c1d01264251ffa145e543de573145ccec92f2f
Juan A Mangubat <juanmangubat@gmail.com>
2024-03-24 02:20:26 +1100

    Hero changes

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -5,21 +5,14 @@ import './Hero.css';
 
 const Hero = () => (
      <div className='app__hero-2 ' id='hero'>
+          {/* <div className='app__hero-img'>
+               <img  src={images.heroLogoKK} alt="hero logo" />
+          </div> */}
           <div className='app__hero-info'>
-               {/* <h1 className='app__hero-h1'>Kingz Kutz Barber</h1> */}
-               <div className='app__hero-img'>
-                    <img  src={images.heroLogoKK} alt="hero logo" />
-               </div>
+               <h1 className='app__hero-h1'>Serving Royalty One Cut at a Time</h1>
+               <h2 className='app__hero-h2'>Your kingdom for premier barber services</h2>
 
-               <p className="p__opensans app__hero-desc"> 
-                    Welcome to Kingz Kutz 
-               </p>
-               <hr className='style-eight'/>
-
-               <p className="p__opensans app__hero-desc"> 
-                    Your Kingdom for Premier Barber Services 
-                    <br/> Serving Royalty, One Cut at a Time
-               </p>
+            
           </div>
      </div>
 );

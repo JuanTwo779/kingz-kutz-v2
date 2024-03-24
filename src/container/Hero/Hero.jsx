52fc87f6ef4155ca3d61ea7d27156ae7223685ad
Juan A Mangubat <juanmangubat@gmail.com>
2024-03-25 00:30:08 +1100

    Hero page changes

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -5,10 +5,10 @@ import './Hero.css';
 
 const Hero = () => (
      <div className='app__hero-2 ' id='hero'>
-          {/* <div className='app__hero-img'>
-               <img  src={images.heroLogoKK} alt="hero logo" />
-          </div> */}
           <div className='app__hero-info'>
+               <div className='app__hero-img'>
+                    <img  src={images.heroLogoKK} alt="hero logo" />
+               </div>
                <h1 className='app__hero-h1'>Serving Royalty One Cut at a Time</h1>
                <h2 className='app__hero-h2'>Your kingdom for premier barber services</h2>
 

37fc75d97a0c97b708604f7ede8471db7e19c729
Juan A Mangubat <juanmangubat@gmail.com>
2024-04-23 17:18:54 +1000

    hero code clean up

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -6,9 +6,6 @@ import './Hero.css';
 const Hero = () => (
      <div className='app__hero-2' id='hero'>
           <div className='app__hero-info'>
-               {/* <div className='app__hero-img'>
-                    <img  src={images.heroLogoKK} alt="hero logo" />
-               </div> */}
                <hr className='app__hero-bar'/>
                <h1 className='app__hero-h1'>Serving royalty</h1>
                <h2 className='app__hero-h2'>One cut at a time</h2>

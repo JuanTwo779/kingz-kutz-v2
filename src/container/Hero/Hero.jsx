36b8838537ee2dda94a53188a1544007e0b03563
JuanTwo779 <juanmangubat@gmail.com>
2024-05-02 17:36:48 +1000

    text changes and crown icon

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { LuCrown } from "react-icons/lu";
+import { GiCrown } from "react-icons/gi";
+import { GiQueenCrown } from "react-icons/gi";
 
 import './Hero.css';
 
 const Hero = () => (
      <div className='app__hero-2' id='hero'>
-          <div className='app__hero-info'>
-               <hr className='app__hero-bar'/>
+          <div className='app__hero-info'>\
+               <GiQueenCrown />
                <h1 className='app__hero-h1'>Serving royalty</h1>
                <h2 className='app__hero-h2'>One cut at a time</h2>
-               <hr className='app__hero-bar'/>
             
           </div>
      </div>

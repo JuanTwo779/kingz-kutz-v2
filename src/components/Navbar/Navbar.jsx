0cecbc4cfba65ed3c777e6a170da685bfd1a6dad
JuanTwo779 <juanmangubat@gmail.com>
2024-08-29 17:44:35 +1000

    Bootstrap css integration

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { FiInstagram } from 'react-icons/fi';
 import { BsScissors } from 'react-icons/bs'
 
 import images from '../../constants/images';
@@ -15,9 +14,9 @@ const Navbar = () => {
       <div className="rounded-background"></div>
 
       <ul className="app__navbar-links items-left">
-        <li className='p__opensans'><a href="#barber">About</a></li>
-        <li className='p__opensans'><a href="#services">Services</a></li>
-        <li className='p__opensans'><a href="#rules">Rules</a></li>
+        <li><a className='p__opensans' href="#barber">About</a></li>
+        <li><a className='p__opensans' href="#services">Services</a></li>
+        <li><a className='p__opensans' href="#rules">Rules</a></li>
       </ul>
 
 
@@ -28,9 +27,9 @@ const Navbar = () => {
         </div>
 
       <ul className='app__navbar-links items-right'>
-        <li className='p__opensans'><a href="#photos" >Gallery</a></li>
-        <li className='p__opensans'><a href="#contact">Contact</a></li>
-        <li className='p__opensans'><a href="#booking">Book</a></li>
+        <li><a className='p__opensans' href="#photos" >Gallery</a></li>
+        <li><a className='p__opensans' href="#contact">Contact</a></li>
+        <li><a className='p__opensans' href="#booking">Book</a></li>
       
       </ul>
 
@@ -41,12 +40,12 @@ const Navbar = () => {
         <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
           <BsScissors fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
           <ul className='app__navbar-smallscreen_links' onClick={() => setToggleMenu(false)}>
-            <li className='p__opensans'><a href="#barber">About</a></li>
-            <li className='p__opensans'><a href="#services">Services</a></li>
-            <li className='p__opensans'><a href="#rules">Rules</a></li>
-            <li className='p__opensans'><a href="#photos">Photos</a></li>
-            <li className='p__opensans'><a href="#contact">Contact</a></li>
-            <li className='p__opensans'><a href="#booking">Book</a></li>
+            <li><a href="#barber">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#rules">Rules</a></li>
+            <li><a href="#photos">Photos</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#booking">Book</a></li>
           </ul>
         </div>
         )}

c67c43caaa62eeee9ba3cfd9cd582fc318b8bcf3
JuanTwo779 <juanmangubat@gmail.com>
2024-09-19 12:07:01 +1000

    import fixes

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants';

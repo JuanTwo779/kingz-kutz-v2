9a6562c25f97453c18a5ad201dfe3d2a9605e911
juanm <juanmangubat@gmail.com>
2025-03-12 22:18:45 +1100

    Hero comp brighten

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import './Hero.css';
 
 const Hero = () => (
      <div className='app__hero-2' id='hero'>
-          <div className='app__hero-info'>\
+          <div className='app__hero-info'>
                <GiQueenCrown />
                <h1 className='app__hero-h1'>Serving royalty</h1>
                <h2 className='p__cormorant app__hero-h2'>One cut at a time</h2>
